1e47811c9c18376fe4fde31b03fed60f8fbeda77
Saulo Arantes <saulovinicius987@gmail.com>
2019-12-06 00:18:21 -0300

    Fixed minor bugs on header search bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,13 +17,19 @@
           </svg>
         </a>
       </div>
-      <div class="search-bar">
+      <div
+        class="search-bar"
+        @click="toggleSearch"
+      >
         <form
           action=""
           class="search-form"
         >
           <div class="form-group">
-            <div class="search-icon">
+            <div
+              ref="searchLabel"
+              class="search-icon"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -36,13 +42,24 @@
                 />
                 <path d="M15.5 14h-.79l-.28-.27c1.2-1.4 1.82-3.31 1.48-5.34-.47-2.78-2.79-5-5.59-5.34-4.23-.52-7.79 3.04-7.27 7.27.34 2.8 2.56 5.12 5.34 5.59 2.03.34 3.94-.28 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
               </svg>
-              <span>Buscar</span>
+              <span
+                ref="placeholder"
+                class="placeholder"
+              >
+                Buscar
+              </span>
             </div>
             <input
+              ref="searchInput"
               type="text"
               class="search-input"
+              @blur="blurSearch"
             >
-            <div class="search-clear">
+            <div
+              ref="searchClear"
+              class="search-clear"
+              @click="clearSearch"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -50,7 +67,7 @@
                 viewBox="0 0 24 24"
               >
                 <path
-                  opacity=".87"
+                  opacity=".21"
                   fill="none"
                   d="M0 0h24v24H0V0z"
                 />
@@ -116,7 +133,31 @@
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+
+  methods: {
+    toggleSearch() {
+      if (this.$refs.searchLabel.classList.contains('active')) {
+        return
+      }
+
+      this.$refs.searchLabel.classList.toggle('active')
+      this.$refs.searchClear.classList.toggle('active')
+      this.$refs.placeholder.classList.toggle('d-none')
+
+      this.$refs.searchInput.focus()
+    },
+
+    blurSearch() {
+      this.toggleSearch()
+
+      this.$refs.searchInput.blur()
+    },
+
+    clearSearch() {
+      this.$refs.searchInput.value = ''
+    }
+  }
 }
 </script>
 
@@ -144,16 +185,24 @@ export default {
     
     .search-bar {
       min-width: 300px;
+
       .form-group {
         align-items: center;
         border: solid 1px #DBDBDB;
+        border-radius: 5px;
         display: flex;
         position: relative;
+
+        svg {
+          opacity: 0.15;
+        }
         
         input {
+          background: none;
           border: none;
           height: 30px;
-          min-width: 200px;
+          width: 100%;
+          outline: none;
           width: 100%;
         }
 
@@ -161,23 +210,32 @@ export default {
           align-items: center;
           display: flex;
           justify-content: center;
+          padding-right: 15px;
           position: absolute;
-          width: 100%;  
+          width: 300px;
+
+          .placeholder {
+            color: #DBDBDB;
+          }
 
           &.active {
             left: 0;
             position: relative;
-            width: auto;
+            width: initial;
+            padding-left: 15px;
           }
         }
 
         .search-clear {
+          cursor: pointer;
           display: none;
           position: absolute;
           right: 0;
 
           &.active {
             display: block;
+            background-color: white;
+            padding: 0 15px;
           }
         }
       }
