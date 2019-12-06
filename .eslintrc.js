19b10dd670f452c868c56c4cef617f3463e32dfc
Saulo Arantes <saulovinicius987@gmail.com>
2019-12-06 13:27:16 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,29 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
+  "env": {
+    "browser": true,
+    "es6": true,
   },
-  'extends': [
+  "extends": [
     "plugin:vue/recommended",
   ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
   },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
   },
-  'rules': {
-    'semi': [2, 'never'],
-    'arrow-parens': 0,
-    'linebreak-style': 0,
-    'indent': ['error', 2]
+  "rules": {
+    "arrow-parens": 0,
+    "eol-last": ["error", "always"],
+    "indent": ["error", 2],
+    "linebreak-style": 0,
+    "semi": [2, "never"],
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "asyncArrow": "always",
+      "named": "never",
+    }],
   },
 }
