41f490ad0d71e49d257c17019e0679f69e334821
Saulo Arantes <saulovinicius987@gmail.com>
2019-12-09 20:27:04 -0300

    Adding description and date on post

--- src/components/Feed/Post.vue
+++ src/components/Feed/Post.vue
@@ -23,13 +23,23 @@
 
     <actions />
 
-    <div class="likes"></div>
+    <div class="likes">
+      <img
+        src="https://instagram.fplu8-1.fna.fbcdn.net/vp/a1887e34947af89e32056e50d74c47fd/5E70F22B/t51.2885-19/s150x150/43913482_751756551832600_1722241345499693056_n.jpg?_nc_ht=instagram.fplu8-1.fna.fbcdn.net"
+        alt=""
+      >
+      <span>Liked by <strong>saulo_arantes</strong> and <strong>42</strong> others</span>
+    </div>
 
-    <div class="description"></div>
+    <div class="description">
+      <strong>saulo_arantes</strong> Desenvolvendo a página inicial do Instagram em Vue JS
+    </div>
 
     <div class="comments"></div>
 
-    <div class="date"></div>
+    <div class="date">
+      3 minutes ago
+    </div>
   </div>
 </template>
 
@@ -89,6 +99,33 @@ export default {
     img {
       width: 100%;
     }
+  }
+
+  .likes {
+    align-items: center;
+    padding: .3rem 18px;
+    display: flex;
+    img {
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      margin-right: 5px;
+    }
+    span {
+      font-size: 14px;
+    }
+  }
+
+  .description {
+    padding: .3rem 18px;
+  }
+
+  .date {
+    padding: .3rem 18px;
+    letter-spacing: .2px;
+    color: #999999;
+    font-size: 10px;
+    text-transform: uppercase;
   }
 }
 </style>
